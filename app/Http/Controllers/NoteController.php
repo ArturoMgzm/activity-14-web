6ff2ad76876f4b403c042992edf10e9921e65e18
Arturo Manrique <arturomgzm@hotmail.com>
2024-05-12 13:02:57 -0600

    first commit

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $request->validate([ 
+            'title' => 'required|string', 
+            'author' => 'required|string', 
+            'timestamp' => 'required|string',
+            'body' => 'required|string',
+            'classification' => 'required|string' 
+
+        ]); 
+            
+        Note::create([
+            'title' => $request->title,
+            'author' => $request->author,
+            'timestamp' => $request->timestamp,
+            'body' => $request->body,
+            'classification' => $request->classification
+        ]); 
+            
+        return response()->json([ 
+            'message' => 'Successfully created note!' 
+        ], 201); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Note $note)
+    {
+        //
+    }
+}
